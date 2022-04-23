ee78b2efa58d7832d83652104b9611a9e082a87b
Morgoliath <93814812+Morgoliath@users.noreply.github.com>
2022-04-23 17:03:55 +0300

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -4,7 +4,6 @@ amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -aG docker $USER
-bash
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
